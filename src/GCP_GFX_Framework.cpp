02432fe9bd66918b7cff53bc78274bfed9ea307a
RanakOFour <4c3Quack@protonmail.com>
2025-11-10 19:20:18 +0000

    GCPFramework::Init

--- src/GCP_GFX_Framework.cpp
+++ src/GCP_GFX_Framework.cpp
@@ -366,17 +366,33 @@ bool GCP_Framework::Init( glm::ivec2 screenSize )
 	// You can experiment with the numbers to see what they do
 	int winPosX = 100;
 	int winPosY = 100;
+	int winWidth = _screenSize.x;
+	int winHeight = _screenSize.y;
 	_SDLwindow = SDL_CreateWindow("My Window!!!",  // The first parameter is the window title
 		winPosX, winPosY,
-		_screenSize.x, _screenSize.y,
+		winWidth, winHeight,
 		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+	// The last parameter lets us specify a number of options
+	// Here, we tell SDL that we want the window to be shown and that it can be resized
+	// You can learn more about SDL_CreateWindow here: https://wiki.libsdl.org/SDL_CreateWindow?highlight=%28\bCategoryVideo\b%29|%28CategoryEnum%29|%28CategoryStruct%29
+	// The flags you can pass in for the last parameter are listed here: https://wiki.libsdl.org/SDL_WindowFlags
 
+	// The SDL_CreateWindow function returns an SDL_Window
+	// This is a structure which contains all the data about our window (size, position, etc)
+	// We will also need this when we want to draw things to the window
+	// This is therefore quite important we don't lose it!
+
+	// The SDL_Renderer is a structure that handles rendering
+	// It will store all of SDL's internal rendering related settings
+	// When we create it we tell it which SDL_Window we want it to render to
+	// That renderer can only be used for this window
+	// (yes, we can have multiple windows - feel free to have a play sometime)
 	SDL_Renderer* renderer = SDL_CreateRenderer(_SDLwindow, -1, 0);
+
 
 	// Now that the SDL renderer is created for the window, we can create an OpenGL context for it!
 	// This will allow us to actually use OpenGL to draw to the window
 	_SDLglcontext = SDL_GL_CreateContext(_SDLwindow);
-	printf("SDL_GL Context created\n");
 
 	// Call our initialisation function to set up GLEW and print out some GL info to console
 	if (!InitGL())
@@ -398,27 +414,14 @@ bool GCP_Framework::Init( glm::ivec2 screenSize )
 	ImGui_ImplOpenGL3_Init(glslVersion);
 
 	// Create the vertex array object for our triangle
-	CreateTriangleVAO(&_triangleVAO, &_vboId);
-
-	printf("Created triangles in VBO %i in VAO %i\n", _vboId, _triangleVAO);
+	_triangleVAO = CreateTriangleVAO();
 
 	// Create the shaders and link them together into the shader program
-	_shaderProgram = LoadShaders("./resources/shaders/ScreenVertex.txt", "./resources/shaders/ScreenFragment.txt");
+	_shaderProgram = LoadShaders("./resources/shaders/VertShader.txt", "./resources/shaders/FragShader.txt");
 
-	_mainBuffer = new Framebuffer(_screenSize.x, _screenSize.y);
+	_mainBuffer = new Framebuffer(winWidth, winHeight);
 
 	_mainBuffer->SetAllPixels(glm::vec3(0, 0, 0));
-
-	// Setting up the GUI system
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-	ImGui::StyleColorsDark();
-
-	const char* glslVersion = "#version 130";
-	ImGui_ImplSDL2_InitForOpenGL(_SDLwindow, _SDLglcontext);
-	ImGui_ImplOpenGL3_Init(glslVersion);
 
 	glEnable(GL_DEPTH_TEST);
 
